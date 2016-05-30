bbee39faf432da09fdd16f5b4c4094a32bcdc18d
guanwill <will@Wills-MacBook-Air.local>
2016-05-30 20:34:54 +1000

    updated edit form

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,11 @@ class Account < ActiveRecord::Base
     has_secure_password
     validates :name, :uniqueness => true
     validates :lol_username, presence: true
-    validates :email, :uniqueness => true
-    validates_confirmation_of :password
+    # validates :email, :uniqueness => true
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+    validates_confirmation_of :password, allow_nil: true
     validates :fav_champion, presence: true
 end
